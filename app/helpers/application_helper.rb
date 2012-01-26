973c2415566997811f83f19f16127e7306c8f797
sturam <sturam75@gmail.com>
2012-01-26 14:59:11 +0000

    done with Sign in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,17 @@ module ApplicationHelper
 
   # Return a title on a per-page basis.
   def title
-    base_title = "webapp brought to by csqaured.co"
+    base_title = "webapp brought to by csquared.co"
     if @title.nil?
       base_title
     else
       "#{base_title} | #{@title}"
     end
   end
+
+  def logo
+	image_tag("c.png", :alt => "Sample App", :class => "round")
+  end
+
 end
 
